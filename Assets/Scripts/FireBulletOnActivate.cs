f877b13a2fcafb43bfd97477e37e6090584d861c
Luis Amado <lamadoalvarez@gmail.com>
2024-04-21 19:01:22 -0600

    Tutorial de la maestra terminado

---
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+
+    public AudioClip bulletAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        AudioSource.PlayClipAtPoint(bulletAudio, Camera.main.transform.position, 0.5f);
+        Destroy(spawnedBullet, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

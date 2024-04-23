d7b4454890e024e0254bff64c408f694d588381a
Luis Amado <lamadoalvarez@gmail.com>
2024-04-22 23:39:13 -0600

    No se que paso

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+
+    public Transform player;
+    public GameObject monster;
+    public DaytimeController daytime;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while(true)
+        {
+            if(daytime.IsNight())
+            {
+                Vector2 pos = Random.insideUnitCircle;
+                Vector3 position = new Vector3(player.position.x + pos.x * 20, transform.position.y, player.position.y + pos.y * 20);
+
+                GameObject spawned = Instantiate(monster, position, Quaternion.identity, transform);
+                spawned.GetComponent<MonsterBehaviour>().player = player;
+            }
+            yield return new WaitForSeconds(Random.Range(1.0f, 3.0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
